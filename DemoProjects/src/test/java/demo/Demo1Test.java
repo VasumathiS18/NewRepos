6e7e78cff19a5c32e6b1ee711484d2efab87ac48
VasumathiS18 <vasumadhan10@gmail.com>
2023-07-14 11:55:33 +0530

    1st commit

---
+++ DemoProjects/src/test/java/demo/Demo1Test.java
@@ -0,0 +1,24 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class Demo1Test {
+	@Test
+public void firstTest()
+{
+	System.out.println("Hi this is running in cmd prompt");
+	
+	/*
+	 * WebDriver driver; WebDriverManager.chromedriver().setup(); driver=new
+	 * ChromeDriver(); driver.get("https://www.google.com");
+	 * 
+	 * System.out.println("Thank you");
+	 */
+	 
+}
+}
